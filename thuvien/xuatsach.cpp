0a1920173113801c59960c7901445825c410f9ea
duykidanh <101781715+duykidanh@users.noreply.github.com>
2022-04-27 08:29:43 +0700

    Update xuatsach.cpp

--- thuvien/xuatsach.cpp
+++ thuvien/xuatsach.cpp
@@ -1,11 +1,12 @@
 #include"header.h"
-//Xuat DS Sach
+//Xuat thong tin Sach
 void Output(Book a, int n)
 {
 	upperAll(a.MaSach);
 	printf("\n|%-3d|%-7s|%-45s|%-20s|%-18s|%-5d|%-10s|%-4d|", n, a.MaSach, a.TenSach, a.TacGia, a.TheLoai, a.NamXB, a.NhaXuatBan, a.SoLuong);
 	printLine(1);
 }
+//xuat DS sach
 void listBook(Book a[], int n)
 {
 	Contents(1);
